eca6c1ed6b8801641f54beb33fd2d6228ecd64dd
tatianalobatsevich <0800501t@gmail.com>
2024-10-30 22:48:25 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function () {
+output.innerHTML = 'Мама, я тебя люблю 💝';
+});
